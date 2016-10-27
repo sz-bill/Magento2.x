c599191ec779095736279d3da3d1cc2d0f8a55a0
sz-bill <414243927@qq.com>
2016-10-27 15:07:44 +0800

    这一套完整的模块例子，有前台的，也有后台的

---
+++ 模块案例/example1/app/code/Ashsmith/Blog/Block/PostList.php
@@ -0,0 +1,63 @@
+<?php
+namespace Ashsmith\Blog\Block;
+
+use Ashsmith\Blog\Api\Data\PostInterface;
+use Ashsmith\Blog\Model\ResourceModel\Post\Collection as PostCollection;
+
+class PostList extends \Magento\Framework\View\Element\Template implements
+    \Magento\Framework\DataObject\IdentityInterface
+{
+    /**
+     * @var \Ashsmith\Blog\Model\ResourceModel\Post\CollectionFactory
+     */
+    protected $_postCollectionFactory;
+
+    /**
+     * Construct
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Ashsmith\Blog\Model\ResourceModel\Post\CollectionFactory $postCollectionFactory,
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Ashsmith\Blog\Model\ResourceModel\Post\CollectionFactory $postCollectionFactory,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_postCollectionFactory = $postCollectionFactory;
+    }
+
+    /**
+     * @return \Ashsmith\Blog\Model\ResourceModel\Post\Collection
+     */
+    public function getPosts()
+    {
+        // Check if posts has already been defined
+        // makes our block nice and re-usable! We could
+        // pass the 'posts' data to this block, with a collection
+        // that has been filtered differently!
+        if (!$this->hasData('posts')) {
+            $posts = $this->_postCollectionFactory
+                ->create()
+                ->addFilter('is_active', 1)
+                ->addOrder(
+                    PostInterface::CREATION_TIME,
+                    PostCollection::SORT_ORDER_DESC
+                );
+            $this->setData('posts', $posts);
+        }
+        return $this->getData('posts');
+    }
+
+    /**
+     * Return identifiers for produced content
+     *
+     * @return array
+     */
+    public function getIdentities()
+    {
+        return [\Ashsmith\Blog\Model\Post::CACHE_TAG . '_' . 'list'];
+    }
+
+}
